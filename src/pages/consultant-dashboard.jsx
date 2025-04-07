aa4553987e8548943b477418dc94475de32a8ba4
dharmarumpal <158178903+dharmarumpal@users.noreply.github.com>
2025-04-07 15:15:27 +0100

    cleanup

--- src/pages/consultant-dashboard.jsx
+++ src/pages/consultant-dashboard.jsx
@@ -1,4 +1,4 @@
-// File: ConsultantDashboard.jsx
+
 import React from "react";
 
 const ConsultantDashboard = () => {
--- src/pages/manager-dashboard.jsx
+++ src/pages/manager-dashboard.jsx
@@ -1,4 +1,3 @@
-// File: ManagerDashboard.jsx
 import React from "react";
 
 const ManagerDashboard = () => {
