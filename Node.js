00c649e1c996ae4c9b331d066738aa628061d2fa
qianqing <z36878166@icloud.com>
2025-04-08 22:01:38 +0100

    login and register

---
+++ Node.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345678',
+    database: 'TimeSheetApp'
+});
+
+app.use(bodyParser.json());
+
+db.connect((err) => {
+    if (err) {
+        console.error('database connection failed:', err);
+        return;
+    }
+    console.log('database connection successful');
+});
+
+app.post('/register', async (req, res) => {
+    const { name, email, password, role } = req.body;
+
+    try {
+        const query = 'INSERT INTO Employee (name, email, password, role) VALUES (?, ?, ?, ?)';
+
+        db.query(query, [name, email, password, role], (err, result) => {
+            if (err) {
+                return res.status(500).json({ success: false, message: 'register filed' });
+            }
+            res.status(201).json({ success: true, message: 'register successful' });
+            });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'server error' });
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { name, password } = req.body;
+
+    if (!name || !password) {
+        return res.status(400).json({ success: false, message: 'name and password can not be null' });
+    }
+
+    try {
+        db.query('SELECT * FROM Employee WHERE name = ?', [name], (err, results) => {
+            if (err) {
+                return res.status(500).json({ success: false, message: 'server error' });
+            }
+
+            if (results.length === 0) {
+                return res.status(400).json({ success: false, message: 'name or password error' });
+            }
+
+            const user = results[0];
+
+            if (password === user.password) {
+                return res.status(200).json({ success: true, message: 'login successful' });
+            } else {
+                return res.status(400).json({ success: false, message: 'name or password error' });
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'server error' });
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`server is running at http://localhost:${port}`);
+});
---
+++ src/pages/Login.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import register from "./Register";
+
+const Login = () => {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+
+        const loginData = {
+            name,
+            password,
+        };
+
+        try {
+            const response = await fetch('http://localhost:3000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(loginData),
+            });
+
+            const result = await response.json();
+
+            if (result.success) {
+                setMessage('log in successful！');
+                // navigate('/dashboard');
+            } else {
+                setMessage(result.message);
+            }
+        } catch (error) {
+            setMessage('fail to login, try again later');
+        }
+    };
+
+    return (
+        <div>
+            <h2>login page</h2>
+            <form onSubmit={handleLogin}>
+                <div>
+                    <label>name:</label>
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="please enter your name"
+                        required
+                    />
+                </div>
+                <div>
+                    <label>password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="please enter your password"
+                        required
+                    />
+                </div>
+                <div>
+                    <button type="submit">login</button>
+                </div>
+                <div>
+                    <Link to="/register">register</Link>
+                </div>
+            </form>
+            {message && <p>{message}</p>}
+        </div>
+    );
+};
+
+export default Login;
