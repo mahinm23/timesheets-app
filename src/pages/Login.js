54d5983b1e5fd24f0988239ff6247db65a3c23c9
dharmarumpal <158178903+dharmarumpal@users.noreply.github.com>
2025-04-09 19:01:47 +0100

    css login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import register from "./Register";
+import "./LoginRegister.css";
 
 const Login = () => {
     const [name, setName] = useState('');
---
+++ src/sql/init_tables.sql
@@ -0,0 +1,64 @@
+-- SQL schema for FDM Timesheets Application (Backend & Database Prototype)
+
+CREATE DATABASE TimeSheetsApp;
+
+-- Table: Employee
+CREATE TABLE Employee (
+    employeeId SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT CHECK (role IN ('Consultant', 'Manager', 'Finance', 'Executive')) NOT NULL
+);
+
+-- Table: Timesheet
+CREATE TABLE Timesheet(
+    timeSheetId SERIAL PRIMARY KEY,
+    status TEXT CHECK (status IN ('Draft', 'Submitted', 'Approved', 'Rejected')),
+    weekStarting DATE NOT NULL,
+    weekEnding DATE NOT NULL,
+    createrId INT REFERENCES Employee(employeeId),
+    submitted_at TIMESTAMP
+);
+
+CREATE TABLE rejectedTimesheet(
+    id INT PRIMARY KEY,
+    timeSheetId INT,
+    reason TEXT,
+    rejected_on DATE,
+    reviewer_id INT,
+    FOREIGN KEY (timeSheetId) REFERENCES timeSheet(timeSheetId),
+    FOREIGN KEY (reviewer_id) REFERENCES Employee(employeeId)
+);
+
+
+# -- Table: Notification (optional, for Observer pattern simulation)
+# CREATE TABLE Notification (
+#     notification_id SERIAL PRIMARY KEY,
+#     employee_id INT REFERENCES Employee(employee_id) ON DELETE CASCADE,
+#     message TEXT NOT NULL,
+#     created_at TIMESTAMP DEFAULT NOW(),
+#     is_read BOOLEAN DEFAULT FALSE
+# );
+
+-- Sample data insert (optional for demo purposes)
+INSERT INTO Employee (name, email, password, role) VALUES
+    ('Alice Smith', 'alice@fdm.com', 'hashedpassword1', 'Consultant'),
+    ('Bob Manager', 'bob@fdm.com', 'hashedpassword2', 'Manager');
+
+
+/* Server and test stuff - robert */
+/*
+CREATE DATABASE timesheet_app;
+-- Use database as target for sql commands
+USE timesheet_app;
+
+-- Test Make Timesheet table
+CREATE TABLE IF NOT EXISTS Timesheet (
+        timesheet_id INT AUTO_INCREMENT PRIMARY KEY,
+        employee_id INT NOT NULL,
+        date DATE NOT NULL,
+        hours_worked INT NOT NULL
+        );
+
+*/ 
